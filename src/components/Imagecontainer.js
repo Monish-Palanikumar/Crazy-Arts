f60026c99e5ddb5feeafff013f226f70b05c0316
unknown <monishpalanikumar@gmail.com>
2019-12-26 22:42:41 +0530

    Added grid structure and bootstrap CDN

--- src/components/Imagecontainer.js
+++ src/components/Imagecontainer.js
@@ -5,41 +5,21 @@ export default class ImageContainer extends Component{
      constructor(props)
      {
           super(props)
-          this.state={
-               images:[require("../photos/1.jpg")]
-          }
      }
      
      render()
      {
-
           return(
-               <div>
-                    <Img src={this.state.images[0]} style={imgStyle} alt="images" />
+               <div className="col-xs-12 col-md-6 col-lg-3 mt-5">
+                    <Img src={this.props.image} style={imgStyle} alt="images" />
                </div>
 
           )
      }
 }
 
-// const Imgcontainer=
-// {
-//      boxShadow: "2px 0px 1px grey",
-//      height: "200px",
-//      borderStyle: "solid",
-//      borderColor: "#6b6c50",
-//      width: "290px",
-//      color: "white",
-//      borderRadius: "15px",
-// };
-
 const imgStyle ={
      width:'300px',
      height:'250px',
      borderRadius:'50px',
-     padding:'20px',
-     filter:'grayscale(75%)',
-     transition: 'ls',
-
-     //border:'1px grey dashed'
 }
--- src/components/Imagetray.js
+++ src/components/Imagetray.js
@@ -1,34 +1,31 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import ImageContainer from './Imagecontainer'
 
-export default class Imagetray extends Component{
-     componentDidMount()
-     {
-          let i = 1;
-          for (i = 1; i <= 40; i++) {
-               const url = "../photos/1.jpg";
-               this.setState(previousState => ({Images:[...previousState.Images,url]}))
-          }
-     }
-     constructor(props)
-     {
+export default class Imagetray extends Component {
+     constructor(props) {
           super(props);
-          const url="../photos/1.jpg"
+          const url = "../photos/1.jpg"
           this.state = {
                Visibility: false,
                Image: require("../photos/1.jpg"),
                Images: []
           }
      }
-     render()
-     {
-          const images=this.state.Images;
-          
-          
-          return(
-               <div style={{padding:'50px'}}>
-                    <ImageContainer image={this.state.Image}/>
-                    {/* {images.map(Image =>(<ImageContainer image={Image} />))}} */}
+     componentWillMount() {
+          let i = 1;
+          const images = []
+          for (i = 1; i <= 40; i++) {
+               images.push(require(`../photos/${i}.jpg`))
+          }
+          this.setState({
+               Images: images
+          })
+     }
+     render() {
+          const images = this.state.Images;
+          return (
+               <div className="row">
+                    {images.map((Image, idx) => <ImageContainer key={idx} image={Image} />)}
                </div>
           )
      }
